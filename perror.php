5c1310cdaf62fdb6855a980ae4a205c75640b865
jasydong <jasydong@qq.com>
2016-12-24 02:20:06 -0800

    添加PHP错误处理代码实例

---
+++ perror.php
@@ -0,0 +1,43 @@
+<?php
+
+error_reporting(E_ALL);
+
+ini_set("display_errors", false);
+
+
+//×¢²áÒì³£´¦Àí
+register_shutdown_function(function() {
+    $error = error_get_last();
+
+    if (!empty($error)) {
+        echo '<p style="color:red;">shutdown:: '.$error['message'].', '.$error['file'].':'.$error['line'].'</p>';
+    }
+});
+
+set_error_handler(function($code, $error, $file = null, $line = null) {
+	if (error_reporting() & $code) {
+		debug_print_backtrace();
+        return true;
+	}
+
+	return true;
+});
+
+set_exception_handler(function($e) {
+	if ($e)	{
+        echo 'exception::';
+		print_r($e);
+        return true;
+	}
+});
+
+echo '<pre>';
+
+//print_r($a);
+
+trigger_error("Cannot divide by zero", E_USER_ERROR);
+
+//throw new Exception('DOH!!');
+
+
+
