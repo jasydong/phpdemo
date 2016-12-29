4a55537dab08fa021586b0939a966be3576e75f3
Jasy Dong <jasydong@qq.com>
2016-12-29 17:18:19 +0800

    优化错误输出

--- perror.php
+++ perror.php
@@ -14,15 +14,69 @@ register_shutdown_function(function() {
     $error = error_get_last();
 
     if (!empty($error)) {
-        echo '<p style="color:red;">Fatal Error:: '.$error['message'].', '.$error['file'].':'.$error['line'].'</p>';
+		output_errorlog($error);
     }
 });
 
 //可以捕获E_WARNING,E_NOTICE,E_USER_ERROR,E_USER_WARNING,E_USER_NOTICE, E_RECOVERABLE_ERROR
 //但无法捕获下列错误类型E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING
 //参考: http://php.net/set_error_handler
-set_error_handler(function($code, $error, $file = null, $line = null) {
+set_error_handler(function($code, $message, $file = null, $line = null) {
+	//参考: http://php.net/error_reporting
+	if (error_reporting() & $code) {
+		$error = array();
+		$error['type'] = $code;
+		$error['message'] = $message;
+		$error['file'] = $file;
+		$error['line'] = $line;
+		output_errorlog($error);
+
+        return true;
+	}
+
+	return true;
+});
+
+//一旦异常被捕获, 程序将停止执行
+//参考: http://php.net/set_exception_handler
+set_exception_handler(function($e) {
+	if ($e)	{
+		$error = array();
+		$error['type'] = $e->getCode();
+		$error['message'] = $e->getMessage();
+		$error['file'] = $e->getFile();
+		$error['line'] = $e->getLine();
+		output_errorlog($error);
+        return true;
+	}
+});
+
+echo '<pre>';
+
+//$a = array(1,2,3);
+print_r($a);
+//exit('I have exited!');
+
+//参考: http://php.net/trigger_error
+if ($divisor == 0) {
+    trigger_error("Cannot divide by zero", E_USER_ERROR);
+}
+
+//try ... catch
+try {
+    $file = fopen("filenoexists.txt", "r");
+    //$aa = new Nothing();
+	//参考: http://php.net/manual/zh/class.exception.php
+    throw new Exception('A Exception Demo');
+} catch (Exception $e) {
+    throw $e;
+}
+
+//错误输出处理
+function output_errorlog($error) {
+	//参考: http://php.net/manual/zh/errorfunc.constants.php
     $errortype = array (
+		0                    => 'Exception',
         E_ERROR              => 'Error',
         E_WARNING            => 'Warning',
         E_PARSE              => 'Parsing Error',
@@ -35,44 +89,17 @@ set_error_handler(function($code, $error, $file = null, $line = null) {
         E_USER_WARNING       => 'User Warning',
         E_USER_NOTICE        => 'User Notice',
         E_STRICT             => 'Runtime Notice',
-        E_RECOVERABLE_ERROR  => 'Catchable Fatal Error'
+        E_RECOVERABLE_ERROR  => 'Catchable Fatal Error',
+		E_DEPRECATED  		 => 'Runtime Info',
+		E_USER_DEPRECATED	 => 'User Trigger Warning',
+		E_ALL  		 		 => 'Any Error Exclude E_STRICT',
     );
 
-	if (error_reporting() & $code) {
-        $type = $errortype[$code];
-        echo "<p style=\"color:gray;\">{$type}:: {$error}</p>";
-        //throw new Exception($error, $code);
-        return true;
+	if (isset($error['type']) && in_array($error['type'], array(E_WARNING,E_NOTICE,E_USER_ERROR,E_USER_WARNING,E_USER_NOTICE,E_USER_DEPRECATED))) {
+		$message = '<p style="color:gray;">['. $errortype[$error['type']].'] '.$error['message'].', '.$error['file'].' (<b>line:'.$error['line'].'</b>)</p>';
+	} else {
+		$message = '<p style="color:red;">['. $errortype[$error['type']].'] '.$error['message'].', '.$error['file'].' (<b>line:'.$error['line'].'</b>)</p>';
 	}
-
-	return true;
-});
-
-//一旦异常被捕获, 程序将停止执行
-//参考: http://php.net/set_exception_handler
-set_exception_handler(function($e) {
-	if ($e)	{
-		print_r($e);
-        return true;
-	}
-});
-
-echo '<pre>';
-
-//$a = array(1,2,3);
-print_r($a);
-
-//参考: http://php.net/trigger_error
-if ($divisor == 0) {
-    trigger_error("Cannot divide by zero", E_USER_ERROR);
+	
+	echo $message;
 }
-
-//
-try {
-    $file = fopen("filenoexists.txt", "r");
-    //$aa = new Nothing();
-    //throw new Exception('DOH!!');
-} catch (Exception $e) {
-    throw $e;
-}
-
